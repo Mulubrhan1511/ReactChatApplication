38d28f3aa87284142124d5def9125cb07bdcbde4
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2025-02-18 17:08:17 +0300

    add Socket client

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,23 @@ import SettingsPage from './pages/SettingsPage'
 import { useAuthStore } from './store/useAuthStore'
 import { Loader } from 'lucide-react'
 import { Toaster } from 'react-hot-toast'
+import { useThemeStore } from './store/useThemeStore'
 
 const App = () => {
-  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
-
+  const {authUser, checkAuth, isCheckingAuth, onlineUsers} = useAuthStore()
+  const {theme} = useThemeStore()
   useEffect(() => {
     checkAuth()
   }, [checkAuth])
 
+  console.log("online Users",onlineUsers)
   if(isCheckingAuth && !authUser) {
     return <div>
       <Loader className='size-10 animate-spin' />
     </div>
   }
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
