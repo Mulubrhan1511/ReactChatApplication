9e3d1aaab3fa32ae724c6eb37d73942e87f6298f
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2025-06-03 16:29:12 +0300

    Update SignUpPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Toaster } from "react-hot-toast";
 
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
-import SignUpPage from "./pages/SignupPage";
+import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from "./pages/ProfilePage";
